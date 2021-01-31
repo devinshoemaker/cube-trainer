1f0e72ac7f6983eb99bac24bf6f22046d444edd1
Devin Shoemaker <devinshoe@gmail.com>
2021-01-30 21:37:47 -0600

    Add Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['apps/cube-trainer/**/*.tsx'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
