83bfbc01a058e2fc7039366fbd8be08ccff8f0c1
Devin Shoemaker <devinshoe@gmail.com>
2023-07-29 10:02:37 -0500

    chore(speed-cube-trainer): add Tailwind

---
+++ apps/speed-cube-trainer/tailwind.config.js
@@ -0,0 +1,26 @@
+const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
+const { join } = require('path');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    join(
+      __dirname,
+      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+    ),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        btn: {
+          background: 'hsl(var(--btn-background))',
+          'background-hover': 'hsl(var(--btn-background-hover))',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
